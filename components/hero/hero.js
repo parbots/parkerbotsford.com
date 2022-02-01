3ca97bdc0e3c498a328dc4bd1bb5feb27707dcb8
Parker Botsford <parker.botsford.dev@gmail.com>
2022-01-31 23:35:35 -0500

    Adjust font sizes

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -4,11 +4,11 @@ const Hero = () => {
     return (
         <div className={styles.hero}>
             <h1>{"Hi, i'm Parker."}</h1>
-            <h2>
+            <p>
                 {
                     "I'm a fullstack web developer with a passion for responsive, accessible, and beautiful designs."
                 }
-            </h2>
+            </p>
         </div>
     );
 };
