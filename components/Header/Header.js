0e63d9de7a7b8bbba5f9fe218a8b18aa579c722a
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-01 15:34:39 -0500

    Remove unused links in Header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,16 +1,9 @@
-import Link from 'next/link';
-
 import styles from './Header.module.css';
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <h2>Parker Botsford</h2>
-            {/* <nav>
-                <Link href='/'>
-                    <a>about</a>
-                </Link>
-            </nav> */}
         </header>
     );
 };
