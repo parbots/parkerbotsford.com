68ef2ed35520ab871794b3f7e1f08d3fce25dd44
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-01 20:41:12 -0500

    Implement basic styling and accessibility on About Section

--- components/AboutSection/AboutSection.js
+++ components/AboutSection/AboutSection.js
@@ -3,12 +3,23 @@ import styles from './AboutSection.module.css';
 const AboutSection = () => {
     return (
         <section className={styles.aboutSection}>
-            <div className={styles.left}>
+            <div className={styles.about}>
                 <h2>About Me</h2>
                 <p>
                     I love building amazing websites that are both fast and
                     responsive.
                 </p>
+            </div>
+            <div className={styles.skills}>
+                <h2>Skills</h2>
+                <ul>
+                    <li>React</li>
+                    <li>Next.js</li>
+                    <li>Git</li>
+                </ul>
+            </div>
+            <div className={styles.info}>
+                <h2>Hover any skill</h2>
             </div>
         </section>
     );
