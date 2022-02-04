1210372c647398d4fce1f7674235d98508077e0b
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-04 12:41:03 -0500

    Create InfoBox component

---
+++ components/InfoBox/InfoBox.js
@@ -0,0 +1,15 @@
+import styles from './InfoBox.module.css';
+
+const InfoBox = (props) => {
+    return (
+        <div className={styles.infoBox}>
+            <div className={styles.top}>
+                <h2 className={styles.title}>{props.title}</h2>
+                <p className={styles.hint}>{props.info}</p>
+            </div>
+            {props.children}
+        </div>
+    );
+};
+
+export default InfoBox;
