a74c3016e18916ec9c19165e3c1b4e43180d533f
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-04 15:46:07 -0500

    Work on skills section

--- components/SkillList/SkillList.js
+++ components/SkillList/SkillList.js
@@ -1,13 +1,63 @@
 import styles from './SkillList.module.css';
 
 import SkillItem from './SkillItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCss3Alt,
+    faGit,
+    faGithub,
+    faHtml5,
+    faJsSquare,
+    faNode,
+    faNpm,
+    faReact,
+} from '@fortawesome/free-brands-svg-icons';
 
 const SkillList = () => {
-    return (
-        <ul className={styles.skillList}>
-            <SkillItem>React</SkillItem>
-        </ul>
-    );
+    const skills = [
+        {
+            name: 'React',
+            icon: <FontAwesomeIcon icon={faReact} size='3x' fixedWidth />,
+        },
+        {
+            name: 'Javascript',
+            icon: <FontAwesomeIcon icon={faJsSquare} size='3x' fixedWidth />,
+        },
+        {
+            name: 'Node.js',
+            icon: <FontAwesomeIcon icon={faNode} size='3x' fixedWidth />,
+        },
+        {
+            name: 'Html',
+            icon: <FontAwesomeIcon icon={faHtml5} size='3x' fixedWidth />,
+        },
+        {
+            name: 'Css',
+            icon: <FontAwesomeIcon icon={faCss3Alt} size='3x' fixedWidth />,
+        },
+        {
+            name: 'Git',
+            icon: <FontAwesomeIcon icon={faGit} size='3x' fixedWidth />,
+        },
+        {
+            name: 'GitHub',
+            icon: <FontAwesomeIcon icon={faGithub} size='3x' fixedWidth />,
+        },
+        {
+            name: 'Npm',
+            icon: <FontAwesomeIcon icon={faNpm} size='3x' fixedWidth />,
+        },
+    ];
+
+    const skillItems = skills.map((skill) => {
+        return (
+            <SkillItem key={skill.name} name={skill.name}>
+                {skill.icon}
+            </SkillItem>
+        );
+    });
+
+    return <ul className={styles.skillList}>{skillItems}</ul>;
 };
 
 export default SkillList;
