c04e525be0bd23dcfbf34d223399e1f8b778eb89
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-04 12:51:05 -0500

    Refactor About Section to use InfoBox and remove SkillsBox and SkillsList

--- components/AboutSection/AboutSection.js
+++ components/AboutSection/AboutSection.js
@@ -1,20 +1,18 @@
 import { useState } from 'react';
+import InfoBox from '../InfoBox/InfoBox';
 
 import styles from './AboutSection.module.css';
-
-import SkillsBox from './SkillsBox/SkillsBox';
 
 const AboutSection = () => {
     return (
         <section className={styles.aboutSection}>
-            <div className={styles.about}>
-                <h2>About Me</h2>
+            <InfoBox title='About Me'>
                 <p>
                     I love building amazing websites that are both fast and
                     responsive.
                 </p>
-            </div>
-            <SkillsBox />
+            </InfoBox>
+            <InfoBox title='Skills' hint='Hover a skill'></InfoBox>
         </section>
     );
 };
--- components/InfoBox/InfoBox.js
+++ components/InfoBox/InfoBox.js
@@ -5,7 +5,7 @@ const InfoBox = (props) => {
         <div className={styles.infoBox}>
             <div className={styles.top}>
                 <h2 className={styles.title}>{props.title}</h2>
-                <p className={styles.hint}>{props.info}</p>
+                <p className={styles.hint}>{props.hint}</p>
             </div>
             {props.children}
         </div>
