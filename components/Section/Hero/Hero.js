fb7dbdabce41619d97425bcd9624af45747033d5
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-06 21:25:30 -0500

    Add typewriting effect

--- components/Section/Hero/Hero.js
+++ components/Section/Hero/Hero.js
@@ -2,12 +2,22 @@ import styles from './Hero.module.css';
 
 import Section from 'components/Section/';
 import Navbar from 'components/Navbar';
+import Typewriter from 'typewriter-effect';
 
 const Hero = () => {
     return (
         <Section id='heroSection' className={styles.heroSection}>
             <Navbar />
-            <h1 className={styles.title}>{"Hi, i'm Parker."}</h1>
+            <h1 className={styles.title}>
+                <Typewriter
+                    onInit={(typeWriter) => {
+                        typeWriter
+                            .pauseFor(500)
+                            .typeString(`Hi, i'm Parker.`)
+                            .start();
+                    }}
+                />
+            </h1>
             <p className={styles.text}>
                 {
                     "I'm a frontend web developer with a passion for creating responsive, accessible, and beautiful designs."
