d7e4f856f198cbd4f10ac4a1e580b0089eb40ee8
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-22 15:00:41 -0500

    Setup basic structure of a project

--- layouts/HomePage/ProjectsSection/ProjectsSection.js
+++ layouts/HomePage/ProjectsSection/ProjectsSection.js
@@ -6,7 +6,28 @@ const ProjectsSection = () => {
             <div className={styles.header}>
                 <h2 className={styles.headerTitle}>Projects</h2>
             </div>
-            <div className={styles.content}></div>
+            <div className={styles.content}>
+                <section className={styles.project}>
+                    <div className={styles.projectImage}>PlaceHolder</div>
+                    <div className={styles.projectInfo}>
+                        <h3 className={styles.projectTitle}>Project Name</h3>
+                        <p>Description</p>
+                        <div className={styles.tagContainer}>
+                            <p className={styles.tagName}>Languages</p>
+                            <ul className={styles.tagList}>
+                                <li className={styles.tag}>Javascript</li>
+                                <li className={styles.tag}>Html</li>
+                            </ul>
+                        </div>
+                        <div className={styles.tagContainer}>
+                            <p className={styles.tagName}>Tools</p>
+                            <ul className={styles.tagList}>
+                                <li className={styles.tag}>Next.js</li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </section>
     );
 };
