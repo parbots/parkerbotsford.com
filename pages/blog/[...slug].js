7d6da56301d93b921729e74fc51b8eecfecffe09
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-24 12:29:10 -0500

    Remove post API functionality

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -5,16 +5,11 @@ import Head from 'next/head';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-import { gql } from '@apollo/client';
-import { contentClient } from 'apollo-client';
-
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-
-const BlogPostPage = ({ post }) => {
+const BlogPostPage = () => {
     return (
         <div className={styles.page}>
             <Head>
-                <title>{post.title}</title>
+                <title>A Blog Post</title>
                 <meta
                     name='description'
                     content='I post about web development. Mostly React and Next.js, but sometimes other stuff too!'
@@ -22,58 +17,11 @@ const BlogPostPage = ({ post }) => {
             </Head>
             <Header />
             <main className={styles.postsSection}>
-                <h1>{post.title}</h1>
-                {documentToReactComponents(post.content.json)}
+                <h1>Post</h1>
             </main>
             <Footer />
         </div>
     );
 };
-
-export async function getStaticProps({ params }) {
-    const { data } = await contentClient.query({
-        query: gql`
-            query {
-                postCollection(where: {slug: "${params.slug}"}) {
-                    items {
-                        title
-                        published
-                        slug
-                        content {
-                            json
-                        }
-                    }
-                }
-            }
-        `,
-    });
-
-    return {
-        props: {
-            post: data.postCollection.items[0],
-        },
-    };
-}
-
-export async function getStaticPaths() {
-    const { data } = await contentClient.query({
-        query: gql`
-            query {
-                postCollection {
-                    items {
-                        slug
-                    }
-                }
-            }
-        `,
-    });
-
-    const posts = data.postCollection.items;
-
-    return {
-        paths: posts.map(({ slug }) => `/blog/${slug}`),
-        fallback: false,
-    };
-}
 
 export default BlogPostPage;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,43 +1,11 @@
 import styles from 'styles/BlogHomepage.module.css';
 
 import Head from 'next/head';
-import Link from 'next/link';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-import { gql } from '@apollo/client';
-import { contentClient } from 'apollo-client';
-
-const BlogHomepage = (props) => {
-    const formatDate = (published) => {
-        return new Date(published).toDateString();
-    };
-
-    const posts =
-        props.posts.length == 0 ? (
-            <p>{`Sorry, I haven't posted yet :(`}</p>
-        ) : (
-            props.posts.map((post) => {
-                return (
-                    <li key={post.title}>
-                        <Link href={`/blog/${post.slug}`}>
-                            <a className={styles.postLink}>
-                                <div className={styles.postTop}>
-                                    <h2 className={styles.postTitle}>
-                                        {post.title}
-                                    </h2>
-                                    <p className={styles.postDate}>
-                                        {formatDate(post.published)}
-                                    </p>
-                                </div>
-                            </a>
-                        </Link>
-                    </li>
-                );
-            })
-        );
-
+const BlogHomepage = () => {
     return (
         <div className={styles.page}>
             <Head>
@@ -50,33 +18,11 @@ const BlogHomepage = (props) => {
             <Header />
             <main className={styles.postsSection}>
                 <h1 className={styles.title}>Posts</h1>
-                <ul className={styles.postList}>{posts}</ul>
+                <ul className={styles.postList}></ul>
             </main>
             <Footer />
         </div>
     );
 };
-
-export async function getStaticProps() {
-    const { data } = await contentClient.query({
-        query: gql`
-            query {
-                postCollection {
-                    items {
-                        title
-                        published
-                        slug
-                    }
-                }
-            }
-        `,
-    });
-
-    return {
-        props: {
-            posts: data.postCollection.items,
-        },
-    };
-}
 
 export default BlogHomepage;
