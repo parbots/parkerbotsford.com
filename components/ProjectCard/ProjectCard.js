dc5b3eba050cebd6854753294baddf14165d1161
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-24 16:48:16 -0500

    Create ProjectCard component

---
+++ components/ProjectCard/ProjectCard.js
@@ -0,0 +1,60 @@
+import styles from './ProjectCard.module.css';
+
+const ProjectCard = ({ name, link, description, madeWith }) => {
+    const toolItems = madeWith.map((tool) => {
+        return (
+            <li key={tool.name} className={styles.toolItem}>
+                {tool.link && (
+                    <a
+                        href={tool.link}
+                        target='_blank'
+                        rel='noreferrer'
+                        className={styles.tool + ' ' + styles.toolLink}
+                    >
+                        {tool.name}
+                    </a>
+                )}
+                {!tool.link && <p className={styles.tool}>{tool.name}</p>}
+            </li>
+        );
+    });
+
+    return (
+        <section className={styles.card}>
+            <div className={styles.temp}>
+                <h3>Image</h3>
+            </div>
+            <div className={styles.projectInfo}>
+                <header className={styles.header}>
+                    <h3 className={styles.title}>
+                        <a
+                            href={link}
+                            target='_blank'
+                            rel='noreferrer'
+                            className={styles.titleLink}
+                        >
+                            {name}
+                        </a>
+                    </h3>
+                    <span className={styles.brace}>{'{'}</span>
+                </header>
+
+                <section className={styles.section}>
+                    <p className={styles.sectionTitle}>description: </p>
+                    <p className={styles.description}>{description}</p>
+                </section>
+
+                <section className={styles.section}>
+                    <p className={styles.sectionTitle}>madeWith:</p>
+                    <ul className={styles.toolsList}>{toolItems}</ul>
+                </section>
+
+                <footer className={styles.footer}>
+                    <span className={styles.brace}>{'}'}</span>
+                </footer>
+            </div>
+        </section>
+    );
+};
+
+export default ProjectCard;
--- layouts/HomePage/ProjectsSection/ProjectsSection.js
+++ layouts/HomePage/ProjectsSection/ProjectsSection.js
@@ -1,3 +1,4 @@
+import ProjectCard from 'components/ProjectCard';
 import styles from './ProjectsSection.module.css';
 
 const ProjectsSection = () => {
@@ -7,26 +8,15 @@ const ProjectsSection = () => {
                 <h2 className={styles.headerTitle}>Projects</h2>
             </div>
             <div className={styles.content}>
-                <section className={styles.project}>
-                    <div className={styles.projectImage}>PlaceHolder</div>
-                    <div className={styles.projectInfo}>
-                        <h3 className={styles.projectTitle}>Project Name</h3>
-                        <p>Description</p>
-                        <div className={styles.tagContainer}>
-                            <p className={styles.tagName}>Languages</p>
-                            <ul className={styles.tagList}>
-                                <li className={styles.tag}>Javascript</li>
-                                <li className={styles.tag}>Html</li>
-                            </ul>
-                        </div>
-                        <div className={styles.tagContainer}>
-                            <p className={styles.tagName}>Tools</p>
-                            <ul className={styles.tagList}>
-                                <li className={styles.tag}>Next.js</li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
+                <ProjectCard
+                    name='Pantry'
+                    link='https://github.com/parbots/pantry-web'
+                    description='All your ingredients, in one place.'
+                    madeWith={[
+                        { name: 'Typescript' },
+                        { name: 'React', link: 'https://reactjs.org/' },
+                    ]}
+                />
             </div>
         </section>
     );
