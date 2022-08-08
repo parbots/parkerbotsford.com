489f91d1cc8e3986de2ea167e9ddb7a99ad9b26d
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-08 18:42:10 -0400

    Add initial about me content

--- layouts/HomePage/AboutSection/AboutSection.js
+++ layouts/HomePage/AboutSection/AboutSection.js
@@ -6,7 +6,21 @@ const AboutSection = () => {
             <header className={styles.header}>
                 <h2 className={styles.headerTitle}>About Me</h2>
             </header>
-            <section className={styles.content}></section>
+            <section className={styles.content}>
+                <p>I love coding and creating tools for other developers.</p>
+
+                <p>(Check them out on my Github!)</p>
+
+                <p>{"I'm currently using Typescript, React, and Next.js."}</p>
+
+                <p>Some fun facts about me:</p>
+
+                <ul className={styles.factList}>
+                    <li>{"I'm obssessed with the Gruvbox colorscheme."}</li>
+                    <li>I love skateboarding!</li>
+                    <li>I drink 2 cups of coffee a day.</li>
+                </ul>
+            </section>
         </section>
     );
 };
