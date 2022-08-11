f2bbdb76199196322db9d93f43b26758f7e6ef29
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-10 22:22:48 -0400

    Add skills section to about section

--- layouts/HomePage/AboutSection/AboutSection.js
+++ layouts/HomePage/AboutSection/AboutSection.js
@@ -1,56 +1,66 @@
 import styles from './AboutSection.module.css';
 
+// TODO: use flex column on mobile
+
 const AboutSection = () => {
     return (
         <section className={styles.section}>
-            <header className={styles.header}>
-                <h2 className={styles.headerTitle}>About Me</h2>
-            </header>
-            <section className={styles.content}>
-                <div>
-                    <p>
-                        I love coding and creating tools for other developers.
-                    </p>
-                    <p>
-                        (Check them out on my{' '}
-                        <a
-                            href='https://www.github.com/parbots'
-                            target='_blank'
-                            rel='noreferrer'
-                            className={styles.aboutLink}
-                        >
-                            Github
-                        </a>
-                        !)
-                    </p>
-                </div>
-
-                <div>
-                    <p>
-                        {"I'm currently using Typescript, React, and Next.js."}
-                    </p>
-                </div>
-
-                <div>
-                    <p>Some fun facts about me:</p>
-
-                    <ul className={styles.factList}>
-                        <li>
-                            {"I'm obssessed with the "}
+            <section className={styles.sectionLeft}>
+                <header className={styles.header}>
+                    <h2 className={styles.headerTitle}>About Me</h2>
+                </header>
+                <section className={styles.content}>
+                    <div>
+                        <p>
+                            I love coding and creating tools for other
+                            developers.
+                        </p>
+                        <p>
+                            (Check them out on my{' '}
                             <a
-                                href='https://www.github.com/morhetz/gruvbox'
+                                href='https://www.github.com/parbots'
                                 target='_blank'
                                 rel='noreferrer'
                                 className={styles.aboutLink}
                             >
-                                Gruvbox
+                                Github
                             </a>
-                            {' colorscheme.'}
-                        </li>
-                        <li>I love skateboarding!</li>
-                        <li>I drink 2 cups of coffee a day.</li>
-                    </ul>
-                </div>
+                            !)
+                        </p>
+                    </div>
+                    <div>
+                        <p>
+                            {
+                                "I'm currently using Typescript, React, and Next.js."
+                            }
+                        </p>
+                    </div>
+                    <div>
+                        <p>Some fun facts about me:</p>
+                        <ul className={styles.factList}>
+                            <li>
+                                {"I'm obssessed with the "}
+                                <a
+                                    href='https://www.github.com/morhetz/gruvbox'
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={styles.aboutLink}
+                                >
+                                    Gruvbox
+                                </a>
+                                {' colorscheme.'}
+                            </li>
+                            <li>I love skateboarding!</li>
+                            <li>I drink 2 cups of coffee a day.</li>
+                        </ul>
+                    </div>
+                </section>
+            </section>
+
+            <section className={styles.sectionRight}>
+                <header className={styles.header}>
+                    <h2 className={styles.headerTitle}>My Skills</h2>
+                </header>
             </section>
         </section>
     );
