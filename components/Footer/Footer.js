0b271e6f6b3533510ce2b4225c403c248389ce7e
Parker Botsford <parker.botsford.dev@gmail.com>
2022-08-23 12:36:52 -0400

    Add year to copyright in Footer component

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from './Footer.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p className={styles.copyright}>© Parker Botsford</p>
+            <p className={styles.copyright}>© Parker Botsford 2022</p>
         </footer>
     );
 };
