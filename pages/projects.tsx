cb96f2f9748ae2f059f61479e02f065d856b189b
Parker Botsford <parker.botsford.dev@gmail.com>
2023-02-28 16:20:21 -0500

    Setup basic projects page structure

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -13,6 +13,15 @@ export default function ProjectsPage() {
             </Head>
 
             <div className={styles.page}>
+                <header className={styles.header}>
+                    <h1 className={styles.title}>{"Parker Botsford's Projects"}</h1>
+                </header>
+
+                <main className={styles.main}></main>
+
+                <footer className={styles.footer}>
+                    <p className={styles.copyright}>Copyright © 2023 Parker Botsford</p>
+                </footer>
             </div>
         </>
     );
