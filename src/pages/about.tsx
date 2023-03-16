85445b57c779c5a5da5a86994ae320442d411069
Parker Botsford <parker.botsford.dev@gmail.com>
2023-03-16 15:04:48 -0400

    Use custom favicon

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,8 +12,12 @@ export default function AboutPage() {
         <>
             <Head>
                 <title>About Me</title>
+                <meta name='description' content='Web developer with a passion for designing beautiful and functional user experiences.' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
+                <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+                <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+                <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+                <link rel='manifest' href='/site.webmanifest' />
             </Head>
 
             <div className={styles.page}>
