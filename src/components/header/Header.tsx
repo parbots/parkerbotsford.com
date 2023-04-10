62ad3d0ae664dfa331c40f2373eef8709a0837e3
Parker Botsford <parker.botsford.dev@gmail.com>
2023-04-10 09:55:33 -0400

    fix(Header): format main tsx file

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ type HeaderProps = {
     title: string;
 };
 
-export const Header = ({title}: HeaderProps) => {
+export const Header = ({ title }: HeaderProps) => {
     return (
         <header className={styles.header}>
             <h1 className={styles.title}>{title}</h1>
