eb342ceafa0760e6eb04e4398f24e83e7ca71c15
Parker Botsford <parker.botsford.dev@gmail.com>
2023-09-28 13:24:19 -0400

    feat(main-header): implement navbar

--- src/components/main/header/header.tsx
+++ src/components/main/header/header.tsx
@@ -1,9 +1,32 @@
 import styles from "./header.module.css";
 
+import { HeaderNav } from "@components/main/headerNav";
+
 export const Header = ({ title }: { title: string }) => {
+    const links = [
+        {
+            name: "home",
+            href: "/",
+        },
+        {
+            name: "about",
+            href: "/about",
+        },
+        {
+            name: "projects",
+            href: "/projects",
+        },
+        {
+            name: "contact",
+            href: "/contact",
+        },
+    ];
+
     return (
         <header className={styles.header}>
-            <h1 className={styles.title}>{title}</h1>
+            <h2 className={styles.title}>{title}</h2>
+            <HeaderNav links={links} />
+            <h2>S</h2>
         </header>
     );
 };
---
+++ src/components/main/headerNav/headerNav.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import styles from "./headerNav.module.css";
+
+export const HeaderNav = ({
+    links,
+}: {
+    links: { name: string; href: string }[];
+}) => {
+    const navLinks = links.map((link, idx, arr) => {
+        return (
+            <>
+                <li className={styles.linkItem}>
+                    <Link key={idx} href={link.href} className={styles.link}>
+                        {link.name}
+                    </Link>
+                </li>
+                {idx < arr.length - 1 && (
+                    <li className={styles.linkItem}>
+                        <p className={styles.linkSeperator}>{"/"}</p>
+                    </li>
+                )}
+            </>
+        );
+    });
+
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.linkList}>{navLinks}</ul>
+        </nav>
+    );
+};
