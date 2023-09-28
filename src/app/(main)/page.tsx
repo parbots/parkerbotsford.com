823a200904c5c67b2fc5654f23ce610034b1448c
Parker Botsford <parker.botsford.dev@gmail.com>
2023-09-28 13:20:56 -0400

    refactor(homepage): move homepage into main layout

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@styles/main/page-home.module.css";
+
+export default function HomePage() {
+    return (
+        <section className={styles.section}>
+            <h1 className={styles.title}>Parker Botsford</h1>
+        </section>
+    );
+}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@styles/main/page-home.module.css";
-
-export default function HomePage() {
-    return (
-        <main className={styles.main}>
-            <h1 className={styles.header}>Parker Botsford</h1>
-        </main>
-    );
-}
