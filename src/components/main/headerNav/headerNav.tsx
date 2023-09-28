604f330ae42fc88617abd602eac72f7e11a46501
Parker Botsford <parker.botsford.dev@gmail.com>
2023-09-28 16:48:18 -0400

    fix(headerNav): Add unique key to navLinks

--- src/components/main/headerNav/headerNav.tsx
+++ src/components/main/headerNav/headerNav.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import styles from "./headerNav.module.css";
+import { v4 as uuid } from "uuid";
+import { Fragment } from "react";
 
 export const HeaderNav = ({
     links,
@@ -8,18 +10,18 @@ export const HeaderNav = ({
 }) => {
     const navLinks = links.map((link, idx, arr) => {
         return (
-            <>
+            <Fragment key={uuid()}>
                 <li className={styles.linkItem}>
-                    <Link key={idx} href={link.href} className={styles.link}>
+                    <Link href={link.href} className={styles.link}>
                         {link.name}
                     </Link>
                 </li>
                 {idx < arr.length - 1 && (
-                    <li key={idx + arr.length} className={styles.linkItem}>
+                    <li className={styles.linkItem}>
                         <p className={styles.linkSeperator}>{"/"}</p>
                     </li>
                 )}
-            </>
+            </Fragment>
         );
     });
 
