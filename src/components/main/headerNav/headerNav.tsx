f4a9cb7fbdadc6e3ed51743bff61b9d530e95209
Parker Botsford <parker.botsford.dev@gmail.com>
2023-09-29 17:32:12 -0400

    feat(headerNav): Show current page in bold

--- src/components/main/headerNav/headerNav.tsx
+++ src/components/main/headerNav/headerNav.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./headerNav.module.css";
 import { v4 as uuid } from "uuid";
 import { Fragment } from "react";
+import { usePathname } from "next/navigation";
 
 export const HeaderNav = ({
     links,
 }: {
     links: { name: string; href: string }[];
 }) => {
+    const currentPathname = usePathname();
+
     const navLinks = links.map((link, idx, arr) => {
         return (
             <Fragment key={uuid()}>
                 <li className={styles.linkItem}>
-                    <Link href={link.href} className={styles.link}>
+                    <Link
+                        href={link.href}
+                        className={styles.link}
+                        data-current={currentPathname == link.href}
+                    >
                         {link.name}
                     </Link>
                 </li>
