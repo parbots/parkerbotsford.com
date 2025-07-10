98de0b1964afb3aff4b526aac0e2c3a75c109e6b
Parker Botsford <parker.botsford.dev@gmail.com>
2025-07-10 16:54:54 -0400

    feat(astro): add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  adapter: vercel()
 });
