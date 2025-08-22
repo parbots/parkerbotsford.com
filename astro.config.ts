f948ca6e14a516487e2adc91c4ef3170049dafb9
Parker Botsford <parker.botsford.dev@gmail.com>
2025-08-22 19:56:24 -0400

    feat: create initial base structure

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
-
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), mdx()],
+    integrations: [react()],
     adapter: vercel(),
 });
