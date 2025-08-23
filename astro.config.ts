efdaf9c44bc285bc211e1a31fef9cec917a87fde
Parker Botsford <parker.botsford.dev@gmail.com>
2025-08-23 01:23:59 -0400

    feat: setup project configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,21 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
+import { visualizer } from 'rollup-plugin-visualizer';
+
 export default defineConfig({
+    site: 'https://www.parkerbotsford.com',
     integrations: [react()],
     adapter: vercel(),
+    vite: {
+        plugins: [
+            visualizer({
+                template: 'flamegraph',
+                emitFile: true,
+                filename: 'stats.html',
+            }),
+        ],
+    },
 });
